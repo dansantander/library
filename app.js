7b1458a256fcc4e4c53d3f7db6994f4a189f796a
Stricks1 <gmalheiross@gmail.com>
2020-07-23 17:42:10 -0300

    added buttons to remove book and toggle read/unread on array

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ function Book(title, author, numPages, read) {
   this.numPages = numPages
   this.read = read
   this.info = function() {
-    return `${this.title} by ${this.author}, ${this.numPages}, ${this.read}`
+    return `${this.title} by ${this.author}, ${this.numPages}, ${this.read ? 'Read' : 'Not Read'}`
+  }
+}
+
+Book.prototype.toggleRead = function () {
+  if(this.read) {
+    this.read = false
+  } else {
+    this.read = true
   }
 }
 
@@ -15,9 +23,18 @@ function addBookToLibrary(book) {
 }
 
 function render() {
+  document.getElementById("book-container").innerHTML = "";
   myLibrary.forEach((book, index) => {
     div = document.createElement('DIV');
     div.innerHTML = book.info()
+    btn = document.createElement('BUTTON')
+    btn.innerHTML = 'Remove'
+    btn.setAttribute('onclick', `removeBook(${index})`);
+    div.appendChild(btn)
+    btn2 = document.createElement('BUTTON')
+    btn2.innerHTML = 'Read/Unread'
+    btn2.setAttribute('onclick', `toggleBook(${index})`);
+    div.appendChild(btn2)
     document.getElementById("book-container").appendChild(div);
   }) 
 }
@@ -31,11 +48,22 @@ function createBookWithForm(){
   t = document.getElementById("title").value
   a = document.getElementById("author").value
   p = document.getElementById("numPages").value
-  r = document.getElementById("read").value
+  r = document.getElementById("read").checked
 
   const book = new Book(t, a, p, r );
 
   addBookToLibrary(book)
   render()
   
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1)
+  render()
+}
+
+
+function toggleBook(index) {
+  myLibrary[index].toggleRead()
+  render()
 }
